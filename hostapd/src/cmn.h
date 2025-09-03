8e701e922b14aaa769252ed19f32bd183d14ca1e
Sri Vidya Gunturi <sgunturi@qti.qualcomm.com>
2025-09-03 15:15:53 +0530

    hostapd-extn: Move the common structures into cmn.h
    This is a cosmetic change only. Move the common structures
    from 240mhz.h file to cmn.h.
    Also include the cmn.h file in the minimal patch instead
    of 240mhz.h.
    
    Signed-off-by: Sri Vidya Gunturi <sgunturi@qti.qualcomm.com>
    
    Change-Id: Ib715e3e7f175e4eb83e6bc6fb770bcfa117eae3a

--- hostapd/src/cmn.h
+++ hostapd/src/cmn.h
@@ -15,6 +15,39 @@ enum ieee80211_op_mode;
 struct hostapd_sta_add_params;
 enum bw_type;
 struct hostapd_freq_params;
+struct hostapd_data;
+struct ieee802_11_elems;
+
+struct ieee80211_240mhz_vendor_oper_extn {
+	u8 ccfs1;
+	u8 ccfs0;
+	u16 punct_bitmap;
+	u16 is5ghz240mhz          :1,
+	    bfmess320mhz          :3,
+	    numsound320mhz        :3,
+	    nonofdmaulmumimo320mhz:1,
+	    mubfmr320mhz          :1;
+	u8 mcs_map_320mhz[3];
+} STRUCT_PACKED;
+
+struct ieee80211_240mhz_params_extn {
+	struct ieee80211_240mhz_vendor_oper_extn *eht_240mhz_capab;
+	size_t eht_240mhz_capab_len;
+};
+
+struct  hostapd_sta_add_params_extn {
+	struct ieee80211_240mhz_params_extn params_240mhz;
+};
+
+struct sta_info_extn {
+	struct ieee80211_240mhz_params_extn params_240mhz;
+};
+
+struct ieee802_11_elems_extn {
+	const u8 *eht_240mhz_capab;
+	u8 eht_240mhz_capab_len;
+};
+
 
 #ifndef CONFIG_QCN_EXTN
 
