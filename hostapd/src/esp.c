CodeLinaro-mirror/qsdk_oss_src_qca-extns_wlan-app-extns

commit 152388b440ae85ddba69525661c56e304ee4963b

QSDK:HOSTAPD:hostapd-extn: Add ESP support with vendor command and IE handling
Introduce esp.c to implement Estimated Service Parameters (ESP) support.
Provide hostapd_drv_set_esp_param to configure ESP via vendor cmd.
Add parsing logic for ESP attributes from QCA vendor events and update
hostapd interface state accordingly. Implement hostapd_eid_esp to
generate ESP Information Element in management frames for BE AC.
Also, update Makefile to build esp.o for QCN extensions.

Implement Estimated Service Parameters (ESP) Information Element to
advertise expected network performance metrics to clients.
The ESP IE allows access points to communicate anticipated service
quality parameters including airtime fraction, PPDU duration, and
block acknowledgment window size. Clients use this information for
better network selection and roaming decisions.

Provide hostapd_drv_set_esp_param_extn() to configure ESP parameters
(airtime, PPDU duration, BA window, enable/disable) via QCA vendor
command QCA_NL80211_VENDOR_SUBCMD_SET_WIFI_CONFIGURATION. Implement
hostapd_drv_get_esp_params_extn() to display the current ESP
configuration. Add hostapd_eid_esp_extn() to generate the ESP IE
(EID 255, extension ID 11) for Best Effort access category in beacon
and probe response frames.

Add event handling infrastructure for dynamic ESP parameter updates.
Implement nl80211_parse_esp_params_extn() to parse ESP airtime values
from driver vendor events, extracting Best Effort AC airtime from the
per-AC bitmap. Add hostapd_update_esp_params_extn() to update the
computed airtime in hostapd interface state and trigger beacon refresh
across all BSSs. Provide qca_nl80211_handle_wifi_config_evt_extn() to
handle WiFi configuration vendor events with Multi-Link Operation (MLO)
link ID support for per-link ESP configuration. Extend cmn.h with ESP
API declarations. Also, update Makefile to add esp.o to the qcn_extns
object list.

Patch-dependency:

hostapd/src/esp.c
@@ -1,0 +1,286 @@
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ */
+
+#include "utils/includes.h"
+#include <netlink/genl/genl.h>
+#include "utils/common.h"
+#include "common/qca-vendor.h"
+#include "drivers/driver.h"
+#include "drivers/driver_nl80211.h"
+#include "common/ieee802_11_common.h"
+#include "ap/hostapd.h"
+#include "ap/beacon.h"
+#include "common/wpa_ctrl.h"
+#include "cmn.h"
+#include "esp.h"
+
+
+/**
+ * hostapd_drv_set_esp_param_extn - Set ESP parameter via vendor command
+ * @hapd: Pointer to hostapd_data
+ * @param: Parameter name (esp_airtime, esp_ppdu_dur, esp_ba_window, enable_esp)
+ * @val: Parameter value
+ * Returns: 0 on success, -1 on failure
+ *
+ * Uses QCA_NL80211_VENDOR_SUBCMD_SET_WIFI_CONFIGURATION (generic framework)
+ */
+int hostapd_drv_set_esp_param_extn(struct hostapd_data *hapd, const char *param,
+				   const int val)
+{
+	struct hostapd_iface_extn *iface_extn = &hapd->iface->iface_extn;
+	struct wpa_driver_nl80211_data *drv;
+	struct nlattr *params, *esp_params;
+	struct i802_bss *bss;
+	struct nl_msg *msg;
+	int i, ret;
+
+	if (!param) {
+		wpa_printf(MSG_ERROR, "ESP: Invalid parameters");
+		return -1;
+	}
+
+	if (!hapd->driver || !hapd->driver->send_mlme) {
+		wpa_printf(MSG_ERROR, "ESP: Driver not initialized");
+		return -1;
+	}
+
+	bss = hapd->drv_priv;
+	if (!bss) {
+		wpa_printf(MSG_ERROR, "ESP: BSS not initialized");
+		return -1;
+	}
+	drv = bss->drv;
+
+	msg = nl80211_bss_msg(bss, 0, NL80211_CMD_VENDOR);
+	if (!msg ||
+	    nla_put_u32(msg, NL80211_ATTR_VENDOR_ID, OUI_QCA) ||
+	    nla_put_u32(msg, NL80211_ATTR_VENDOR_SUBCMD,
+			QCA_NL80211_VENDOR_SUBCMD_SET_WIFI_CONFIGURATION))
+		goto fail;
+
+	params = nla_nest_start(msg, NL80211_ATTR_VENDOR_DATA);
+	if (!params)
+		goto fail;
+
+	if (hapd->mld_link_id != NL80211_DRV_LINK_ID_NA &&
+	    nla_put_u8(msg, QCA_WLAN_VENDOR_ATTR_CONFIG_MLO_LINK_ID,
+		       hapd->mld_link_id))
+		goto fail;
+
+	esp_params = nla_nest_start(msg, QCA_WLAN_VENDOR_ATTR_CONFIG_ESP_PARAMS);
+	if (!esp_params)
+		goto fail;
+
+	if (os_strcmp(param, "esp_airtime") == 0) {
+		if (nla_put_u32(msg, QCA_WLAN_VENDOR_ATTR_CONFIG_ESP_AIRTIME,
+				val))
+			goto fail;
+		wpa_printf(MSG_DEBUG, "ESP: Setting airtime=%d", val);
+	} else if (os_strcmp(param, "esp_ppdu_dur") == 0) {
+		if (nla_put_u8(msg, QCA_WLAN_VENDOR_ATTR_CONFIG_ESP_PPDU_DUR,
+			       val))
+			goto fail;
+		wpa_printf(MSG_DEBUG, "ESP: Setting PPDU duration=%d", val);
+	} else if (os_strcmp(param, "esp_ba_window") == 0) {
+		if (nla_put_u8(msg, QCA_WLAN_VENDOR_ATTR_CONFIG_ESP_BA_WINDOW,
+			       val))
+			goto fail;
+		wpa_printf(MSG_DEBUG, "ESP: Setting BA window=%d", val);
+	} else if (os_strcmp(param, "enable_esp") == 0) {
+		if (nla_put_u8(msg, QCA_WLAN_VENDOR_ATTR_CONFIG_ESP_ENABLE,
+			       val))
+			goto fail;
+		wpa_printf(MSG_DEBUG, "ESP: Setting enable=%d", val);
+	} else {
+		wpa_printf(MSG_ERROR, "ESP: Unknown parameter '%s'", param);
+		goto fail;
+	}
+
+	nla_nest_end(msg, esp_params);
+	nla_nest_end(msg, params);
+
+	ret = send_and_recv_cmd(drv, msg);
+	if (ret) {
+		wpa_printf(MSG_ERROR,
+			   "ESP: Failed to send vendor command: %s (%d)",
+			   strerror(-ret), ret);
+		return ret;
+	}
+
+	/* Store values in iface structure after successful driver update */
+	if (os_strcmp(param, "esp_airtime") == 0)
+		iface_extn->esp.airtime = val;
+	else if (os_strcmp(param, "esp_ppdu_dur") == 0)
+		iface_extn->esp.ppdu_dur = val;
+	else if (os_strcmp(param, "esp_ba_window") == 0)
+		iface_extn->esp.ba_window = val;
+	else if (os_strcmp(param, "enable_esp") == 0)
+		iface_extn->esp.enable = val;
+
+	for (i = 0; i < hapd->iface->num_bss; i++)
+		ieee802_11_set_beacon(hapd->iface->bss[i]);
+
+	return 0;
+
+fail:
+	wpa_printf(MSG_ERROR, "ESP: Failed to build vendor command");
+	nlmsg_free(msg);
+	return -1;
+}
+
+
+/**
+ * nl80211_parse_esp_params_extn - Parse and handle ESP parameters from vendor event
+ * @bss: Pointer to struct i802_bss
+ * @esp_params_attr: Nested ESP parameters attribute
+ * @link_id: MLD link id
+ * Returns: 0 on success, -1 on failure
+ *
+ * Parses nested ESP parameters from QCA_WLAN_VENDOR_ATTR_CONFIG_ESP_PARAMS
+ * and updates the corresponding values in hostapd_iface structure.
+ */
+int nl80211_parse_esp_params_extn(struct i802_bss *bss,
+				  struct nlattr *esp_params_attr,
+				  u8 link_id)
+{
+	struct nlattr *esp_params[QCA_WLAN_VENDOR_ATTR_CONFIG_ESP_MAX + 1] = {0};
+	struct esp_update_event *esp_event;
+	union wpa_event_data event;
+	uint32_t esp_airtime, temp;
+	struct nlattr *attr;
+	int rem;
+
+	if (!esp_params_attr) {
+		wpa_printf(MSG_ERROR, "ESP: esp_params_attr is NULL - vendor event handler bug!");
+		return -1;
+	}
+
+	os_memset(&event, 0, sizeof(event));
+	esp_event = &event.event_data_extn.esp_update_event;
+	esp_event->link_id = link_id;
+
+	nla_for_each_nested(attr, esp_params_attr, rem) {
+		int type = nla_type(attr);
+
+		if (type <= QCA_WLAN_VENDOR_ATTR_CONFIG_ESP_MAX)
+			esp_params[type] = attr;
+	}
+
+	if (!esp_params[QCA_WLAN_VENDOR_ATTR_CONFIG_ESP_AIRTIME]) {
+		wpa_printf(MSG_ERROR,
+			   "ESP: QCA_WLAN_VENDOR_ATTR_CONFIG_ESP_AIRTIME not present");
+		return -EINVAL;
+	}
+
+	esp_airtime = nla_get_u32(esp_params[QCA_WLAN_VENDOR_ATTR_CONFIG_ESP_AIRTIME]);
+	wpa_printf(MSG_INFO, "ESP: Successfully parsed! Airtime=%u", esp_airtime);
+
+	/* Update ESP airtime in hostapd interface.
+	 * Air time value is received for all ACs. But only the air time for
+	 * BE AC is required to be filled in the IE. Bits 0 to 7 represents
+	 * air time of BE AC. Strip that and scale it to 255.
+	 */
+	temp = esp_airtime & 0xFF;
+	temp = (temp * 255) / 100;
+	esp_event->airtime = temp;
+
+	wpa_supplicant_event(bss->ctx, EVENT_ESP_UPDATE, &event);
+
+	return 0;
+}
+
+
+/**
+ * hostapd_update_esp_params_extn - update airtime fraction for the hapd interface
+ * @hapd: Pointer to hostapd_data
+ * @data: WPA event data
+ */
+void hostapd_update_esp_params_extn(struct hostapd_data *hapd,
+				    union wpa_event_data *data)
+{
+	struct esp_update_event *esp_event;
+	struct hostapd_data *link_hapd;
+
+	if (!data) {
+		wpa_printf(MSG_ERROR, "WPA event with NULL data");
+		return;
+	}
+
+	esp_event = &data->event_data_extn.esp_update_event;
+
+	link_hapd = switch_link_hapd(hapd, esp_event->link_id);
+	if (!link_hapd)
+		return;
+
+	link_hapd->iface->iface_extn.esp.computed_airtime = esp_event->airtime;
+	wpa_printf(MSG_INFO, "ESP: Updated computed_airtime=%u",
+		   esp_event->airtime);
+}
+
+
+/**
+ * hostapd_esp_ie_len_extn - Get ESP IE length
+ * @hapd: Pointer to hostapd_data
+ * Returns: ESP IE length
+ */
+size_t hostapd_esp_ie_len_extn(struct hostapd_data *hapd)
+{
+	if (!hapd || !hapd->iface || !hapd->iface->iface_extn.esp.enable)
+		return 0;
+
+	return 2 /* IE header */ + 1 /* ext id */ + 3 /* esp info field */;
+}
+
+
+/**
+ * hostapd_eid_esp_extn - Adds Estimated Service Parameters (ESP) IE
+ * @hapd: Pointer to hostapd_data
+ * @eid: Position in buffer
+ * @len: Length of buffer
+ * Returns: Updated pointer of the position in buffer
+ *
+ * Writes a single-AC ESP IE (EID 255, ext 11) and returns updated pointer.
+ */
+u8 * hostapd_eid_esp_extn(struct hostapd_data *hapd, u8 *eid, size_t len)
+{
+	struct hostapd_iface_extn *iface_extn;
+	u8 *pos = eid;
+	u8 head;
+
+	if (!hapd || !hapd->iface || !hapd->iface->iface_extn.esp.enable)
+		return pos;
+
+	iface_extn = &hapd->iface->iface_extn;
+
+	if (len < (size_t)(2 + 1 + 3))
+		return pos;
+
+	/* Element ID Extension */
+	*pos++ = WLAN_EID_EXTENSION;
+	*pos++ = 1 + 3; /* length: ext id + esp info field */
+	*pos++ = WLAN_EID_EXT_ESTIMATED_SERVICE_PARAMS;
+
+	head = 0x1; /* Access Category: BE */
+	head |= (0x3 << 3); /* Data Format: AMPDU and AMSDU enabled */
+	if (iface_extn->esp.ba_window)
+		head |= (iface_extn->esp.ba_window & 0x7) << 5; /* BA Window Size as entered by user */
+	else
+		head |= (0x5 << 5); /* Default BA Window Size of 16 */
+	*pos++ = head;
+
+	/* Airtime fraction */
+	if (iface_extn->esp.airtime)
+		*pos++ = iface_extn->esp.airtime;
+	else
+		*pos++ = iface_extn->esp.computed_airtime;
+
+	/* PPDU duration target */
+	if (iface_extn->esp.ppdu_dur)
+		*pos++ = iface_extn->esp.ppdu_dur;
+	else
+		*pos++ = ESP_DEFAULT_PPDU_DURATION;
+
+	return pos;
+}

hostapd/src/Makefile
@@ -5,4 +5,5 @@ OBJS+= \
 	qcn_extns/driver_nl80211_extn.o \
 	qcn_extns/ap_config_extn.o \
 	qcn_extns/ctrl_iface_extn.o \
-	qcn_extns/drv_callbacks_extn.o
+	qcn_extns/drv_callbacks_extn.o \
+	qcn_extns/esp.o

hostapd/src/cmn.h
@@ -23,6 +23,8 @@ struct wpa_ctrl;
 struct hostapd_bss_config;
 struct i802_bss;
 enum wpa_event_type;
+struct nlattr;
+struct wpa_ctrl;
 union wpa_event_data;
 
 struct ieee80211_240mhz_vendor_oper_extn {
@@ -50,8 +52,18 @@ struct sta_info_extn {
 	struct ieee80211_240mhz_params_extn params_240mhz;
 };
 
+/**
+ * struct esp_update_event - Data for EVENT_ESP_UPDATE
+ * @link_id: Link for which ESP airtime update was received
+ * @airtime: Airtime fraction computed in the firmware
+ */
+struct esp_update_event {
+	u8 link_id;
+	u8 airtime;
+};
+
 union wpa_event_data_extn {
-	/* Add extn wpa_event_data here */
+	struct esp_update_event esp_update_event;
 };
 
 struct ieee802_11_elems_extn {
@@ -65,6 +77,18 @@ struct hostapd_config_extn {
 
 struct hostapd_bss_config_extn {
 	/* Add Per-BSS configuration for extn here */
+};
+
+struct esp_extn {
+	u8 airtime;
+	u8 ppdu_dur;
+	u8 ba_window;
+	u8 enable;
+	u32 computed_airtime;
+};
+
+struct hostapd_iface_extn {
+	struct esp_extn esp;
 };
 
 #ifndef CONFIG_QCN_EXTN
@@ -280,6 +304,25 @@ hostapd_wpa_event_extn(void *ctx, int event,
 	return -1;
 }
 
+static inline int
+qca_nl80211_handle_wifi_config_evt_extn(struct i802_bss *bss,
+					u8 *data, size_t len)
+{
+	return -1;
+}
+
+static inline
+u8 * hostapd_eid_esp_extn(struct hostapd_data *hapd, u8 *eid, size_t len)
+{
+       return eid;
+}
+
+static inline
+size_t hostapd_esp_ie_len_extn(struct hostapd_data *hapd)
+{
+       return 0;
+}
+
 #else
 
 void hostapd_get_oper_center_freq_seg_extn(struct hostapd_config *conf,
@@ -367,12 +410,26 @@ int
 hostapd_config_fill_extn(struct hostapd_config *conf,
 			 struct hostapd_bss_config *bss,
 			 const char *buf, char *pos, int line);
-
 int nl80211_vendor_event_qca_extn(struct i802_bss *bss,
 				  u32 subcmd, u8 *data, size_t len);
 
 int hostapd_wpa_event_extn(void *ctx, enum wpa_event_type event,
 			   union wpa_event_data *data);
+#ifdef HOSTAPD
+struct hostapd_data *
+switch_link_hapd(struct hostapd_data *hapd, int link_id);
+#else
+static inline struct hostapd_data *
+switch_link_hapd(struct hostapd_data *hapd, int link_id)
+{
+    return hapd;
+}
+#endif
+int qca_nl80211_handle_wifi_config_evt_extn(struct i802_bss *bss,
+					    u8 *data, size_t len);
+size_t hostapd_esp_ie_len_extn(struct hostapd_data *hapd);
+u8 * hostapd_eid_esp_extn(struct hostapd_data *hapd, u8 *eid, size_t len);
+size_t hostapd_esp_ie_len_extn(struct hostapd_data *hapd);
 
 #endif /* CONFIG_QCN_EXTN */
 #endif /* CMN_H */

hostapd/src/ctrl_iface_extn.c
@@ -6,7 +6,78 @@
 #include "includes.h"
 #include "utils/common.h"
 #include "ap/hostapd.h"
-#include "cmn.h"
+#include "esp.h"
+#include "utils/os.h"
+#include "common/ieee802_11_defs.h"
+#include "ap/ap_config.h"
+
+
+static int hostapd_ctrl_iface_set_esp_extn(struct hostapd_data *hapd, char *cmd)
+{
+        struct hostapd_iface_extn *iface_extn = &hapd->iface->iface_extn;
+        char *param;
+        int val;
+
+        if (!iface_extn)
+                return -1;
+
+        param = strsep(&cmd, "=");
+        if (!param || !cmd)
+                return -1;
+
+        if (strcmp(param, "enable_esp") != 0 && !iface_extn->esp.enable) {
+                wpa_printf(MSG_ERROR, "ESP feature is not enabled");
+                return -1;
+        }
+
+        val = atoi(cmd);
+
+        if (os_strcmp(param, "esp_airtime") == 0) {
+                if (val < 0 || val > 255) {
+                        wpa_printf(MSG_ERROR, "ESP: Invalid airtime value. Permissible range: 0-255");
+                        return -1;
+                }
+        } else if (os_strcmp(param, "esp_ppdu_dur") == 0) {
+                if (val < 0 || val > 255) {
+                        wpa_printf(MSG_ERROR, "ESP: Invalid PPDU duration value. Permissible range: 0-255");
+                        return -1;
+                }
+        } else if (os_strcmp(param, "esp_ba_window") == 0) {
+                if (val < 0 || val > 7) {
+                        wpa_printf(MSG_ERROR, "ESP: Invalid BA window value. Permissible range: 0-7");
+                        return -1;
+                }
+        }
+
+        return hostapd_drv_set_esp_param_extn(hapd, param, val);
+}
+
+
+static int hostapd_ctrl_iface_get_esp_extn(struct hostapd_data *hapd,
+                                           const char *cmd, char *reply,
+                                           int reply_size)
+{
+        struct hostapd_iface_extn *iface_extn = &hapd->iface->iface_extn;
+        int ret;
+
+        if (!iface_extn)
+                return -1;
+
+        ret = os_snprintf(reply, reply_size,
+                          "airtime=%u "
+                          "ppdu_dur=%u "
+                          "ba_window=%u "
+                          "enable_esp=%u\n",
+                          iface_extn->esp.airtime,
+                          iface_extn->esp.ppdu_dur,
+                          iface_extn->esp.ba_window,
+                          iface_extn->esp.enable);
+        if (os_snprintf_error(reply_size, ret))
+                return -1;
+
+        return ret;
+}
+
 
 int
 hostapd_ctrl_iface_receive_process_extn(struct hostapd_data *hapd,
@@ -15,22 +86,17 @@ hostapd_ctrl_iface_receive_process_extn(struct hostapd_data *hapd,
 					struct sockaddr_storage *from,
 					socklen_t fromlen, int *reply_len)
 {
-/*
-	if (os_strncmp(buf, "TEMP_CMD ", 5) == 0) {
-		reply_len_extn = hostapd_tempcmd_handle_cli(hapd, buf + 5,
-							 reply, reply_size);
-	} else {
+	int reply_len_extn = *reply_len;
 
-* Return -1 if no extension CLI command is parsed.
-* This allows the parent API hostapd_ctrl_iface_receive_process_extn to
-* continue processing its logic.
-*/
-	return -1;
-
-/*
- * Handle as below and Return 0 if an extension configuration is handled.
- * In this case, the parent API hostapd_ctrl_iface_receive_process
- * will return immediately.
+	if (os_strncmp(buf, "SET_ESP", 7) == 0) {
+                if (hostapd_ctrl_iface_set_esp_extn(hapd, buf + 8))
+                        reply_len_extn = -1;
+        } else if (os_strncmp(buf, "GET_ESP", 7) == 0) {
+		reply_len_extn = hostapd_ctrl_iface_get_esp_extn(hapd, buf + 7, reply,
+								 reply_size);
+        } else {
+		return -1;
+	}
 
 	*reply_len = reply_len_extn;
 
@@ -40,5 +106,4 @@ hostapd_ctrl_iface_receive_process_extn(struct hostapd_data *hapd,
 	}
 
 	return 0;
-*/
 }

hostapd/src/driver_nl80211_extn.c
@@ -24,6 +24,7 @@
 #include "common/wpa_common.h"
 #include "drivers/driver.h"
 #include "drivers/driver_nl80211.h"
+#include "esp.h"
 
 
 struct hostapd_sta_add_params;
@@ -128,9 +129,48 @@ int nl80211_vendor_event_qca_extn(struct i802_bss *bss,
 				  u32 subcmd, u8 *data, size_t len)
 {
 	switch (subcmd) {
+	case QCA_NL80211_VENDOR_SUBCMD_GET_WIFI_CONFIGURATION:
+		qca_nl80211_handle_wifi_config_evt_extn(bss, data, len);
+		break;
 	default:
 		return -EINVAL;
 	}
+
+	return 0;
+}
+
+int qca_nl80211_handle_wifi_config_evt_extn(struct i802_bss *bss,
+					    u8 *data, size_t len)
+{
+	struct nlattr *tb[QCA_WLAN_VENDOR_ATTR_CONFIG_MAX + 1] = {0};
+	u8 link_id = 0;
+
+	if (!bss) {
+		wpa_printf(MSG_ERROR, "nl80211: bss is NULL!");
+		return -EINVAL;
+	}
+
+	if (!(data && len)) {
+		wpa_printf(MSG_ERROR,
+			   "nl80211: Invalid data for WiFi configuration event");
+		return -EINVAL;
+	}
+
+	if (nla_parse(tb, QCA_WLAN_VENDOR_ATTR_CONFIG_MAX,
+		      (struct nlattr *)data, len, NULL)) {
+		wpa_printf(MSG_ERROR,
+			   "nl80211: Failed to parse WiFi configuration attributes");
+		return -EINVAL;
+	}
+
+	if (tb[QCA_WLAN_VENDOR_ATTR_CONFIG_MLO_LINK_ID])
+		link_id = nla_get_u8(tb[QCA_WLAN_VENDOR_ATTR_CONFIG_MLO_LINK_ID]);
+
+	/* Check if ESP params are present */
+	if (tb[QCA_WLAN_VENDOR_ATTR_CONFIG_ESP_PARAMS])
+		return nl80211_parse_esp_params_extn(bss,
+						     tb[QCA_WLAN_VENDOR_ATTR_CONFIG_ESP_PARAMS],
+						     link_id);
 
 	return 0;
 }

hostapd/src/drv_callbacks_extn.c
@@ -1,18 +1,26 @@
+// SPDX-License-Identifier: BSD-3-Clause
 /*
  * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
- * SPDX-License-Identifier: BSD-3-Clause
  */
 
 #include "includes.h"
 #include "utils/common.h"
 #include "ap/hostapd.h"
-#include "cmn.h"
+#include "esp.h"
 
 
 int hostapd_wpa_event_extn(void *ctx, enum wpa_event_type event,
 			   union wpa_event_data *data)
 {
+	struct hostapd_data *hapd = ctx;
+
+	if (hapd == NULL)
+                return -EINVAL;
+
 	switch (event) {
+	case EVENT_ESP_UPDATE:
+		hostapd_update_esp_params_extn(hapd, data);
+		break;
 	default:
 		return -EINVAL;
 	}

hostapd/src/esp.h
@@ -1,0 +1,20 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ */
+
+#ifndef ESP_H
+#define ESP_H
+
+#define ESP_DEFAULT_PPDU_DURATION       100
+
+int hostapd_drv_set_esp_param_extn(struct hostapd_data *hapd, const char *param,
+				   const int val);
+int hostapd_drv_get_esp_params_extn(struct hostapd_data *hapd, int *airtime,
+				    int *ppdu_dur, int *ba_window, int *enable);
+int nl80211_parse_esp_params_extn(struct i802_bss *bss,
+				  struct nlattr *esp_params_attr, u8 link_id);
+void hostapd_update_esp_params_extn(struct hostapd_data *hapd,
+				    union wpa_event_data *data);
+
+#endif /* ESP_H */

hostapd/src/hostapd_cli_extn.c
@@ -9,5 +9,32 @@
 #include "common/wpa_ctrl.h"
 #include "common/ieee802_11_defs.h"
 #include "hostapd_cli_extn.h"
+#include "utils/os.h"
 
 /* Add your cli handling for extensions here */
+int hostapd_cli_cmd_set_esp_extn(struct wpa_ctrl *ctrl, int argc,
+				 char *argv[])
+{
+	char buf[128] = {'\0'};
+	int res;
+
+	if (argc != 2) {
+		printf("Invalid 'set_esp' command - usage: set_esp <param> <value>\n");
+		return -1;
+	}
+
+	res = os_snprintf(buf, sizeof(buf), "SET_ESP %s=%s", argv[0], argv[1]);
+	if (os_snprintf_error(sizeof(buf), res)) {
+		printf("Too long SET_ESP command.\n");
+		return -1;
+	}
+
+	return wpa_ctrl_command(ctrl, buf);
+}
+
+
+int hostapd_cli_cmd_get_esp_extn(struct wpa_ctrl *ctrl, int argc,
+				 char *argv[])
+{
+	return wpa_ctrl_command(ctrl, "GET_ESP");
+}

commit e0013cf78253fc04c7039229b07c6eafd44973e9

QSDK:HOSTAPD:hostapd-extn: reduce SET_ESP beacon churn on multi-MLD setups
SET_ESP intermittently times out on 6 GHz when 16 MLDs are configured
because hostapd spends several seconds regenerating beacons. The root
cause is that hostapd_drv_set_esp_param_extn() iterates all BSSes on
the radio and calls ieee802_11_set_beacon() for each one, which cascades
beacon updates across other radios (MLD/6G colocation) and triggers
hundreds of updates per command. The fix is to switch the loop to
ieee802_11_set_beacon_per_bss_only() so only BSSes on the target radio
are refreshed while avoiding cross‑iface cascades and CLI timeouts.

Tag: ESP
Patch-dependency: Id8b873df53faa56d221d37dcd920de1c23e4eaf5
Patch-work: None
Fixes: 152388b440ae ("QSDK:HOSTAPD:hostapd-extn: Add ESP support with
		     vendor command and IE handling")

Change-Id: I8db5ece2a17ddf1370ed74966f73afe54f3f7418
Signed-off-by: Amith A <amitajit@qti.qualcomm.com>

hostapd/src/esp.c
@@ -119,8 +119,17 @@ int hostapd_drv_set_esp_param_extn(struct hostapd_data *hapd, const char *param,
 	else if (os_strcmp(param, "enable_esp") == 0)
 		iface_extn->esp.enable = val;
 
-	for (i = 0; i < hapd->iface->num_bss; i++)
-		ieee802_11_set_beacon(hapd->iface->bss[i]);
+	for (i = 0; i < hapd->iface->num_bss; i++) {
+		struct hostapd_data *bss = hapd->iface->bss[i];
+
+		if (!bss)
+			continue;
+		ieee802_11_set_beacon_per_bss_only(bss);
+#ifdef CONFIG_IEEE80211BE
+		if (bss->conf && bss->conf->mld_ap)
+			hostapd_gen_per_sta_profiles(bss);
+#endif /* CONFIG_IEEE80211BE */
+	}
 
 	return 0;
 