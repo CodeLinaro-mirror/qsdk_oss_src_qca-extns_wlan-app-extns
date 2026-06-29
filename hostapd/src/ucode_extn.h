914860e1d57cb906f676c3dd63ea3777f7f84038
Mohan Thangavel <mohathan@qti.qualcomm.com>
2026-06-29 17:42:15 +0530

    QSDK:CP:hostapd_extn: Add API for wide-band channel checks
    Add new helper APIs to check dfs channel from the full chandef
    
    wpas_ucode_freq_range_is_dfs() ->checks whether any 20 MHz subchannel
    in given center-frequency/bandwidth range overlaps a DFS channel.
    
    wpas_ucode_is_dfs_chandef() -> Uses freq/ch_width/cf1/cf2 to evaluate
    complete chandef and returns true if any subchannel is DFS. Handle
    special wide-band cases,
      80+80 MHz as two separate 80 MHz segments
      160 MHz handling with cf2 when present
      5GHz 240 MHz representation as 160 MHz + 80 MHz
    
    Patch-dependency: NA
    Fixes: NA
    
    Change-Id: I073f37170a4dc809730c531c6fcc7bec873f45ec
    Signed-off-by: Mohan Thangavel <mohathan@qti.qualcomm.com>

--- hostapd/src/ucode_extn.h
+++ hostapd/src/ucode_extn.h
@@ -13,6 +13,17 @@ void hostapd_ucode_trigger_bhsta_disconnect(struct hostapd_iface *hapd)
 {
 }
 static inline
+bool wpas_ucode_freq_range_is_dfs(int center_freq, int bandwidth)
+{
+	return false;
+}
+static inline
+bool wpas_ucode_is_dfs_chandef(int freq, enum chan_width ch_width,
+			       int cf1, int cf2)
+{
+	return false;
+}
+static inline
 void hostapd_ucode_notify_uplink_csa(struct hostapd_iface *hapd, int event, u8 channel,
 				     int freq, int csa_count, u8 new_ch_width,
 				     u8 ch_seg_0, u8 ch_seg_1,
@@ -21,6 +32,9 @@ void hostapd_ucode_notify_uplink_csa(struct hostapd_iface *hapd, int event, u8 c
 }
 #else
 void hostapd_ucode_trigger_bhsta_disconnect(struct hostapd_iface *hapd);
+bool wpas_ucode_freq_range_is_dfs(int center_freq, int bandwidth);
+bool wpas_ucode_is_dfs_chandef(int freq, enum chan_width ch_width,
+			       int cf1, int cf2);
 void hostapd_ucode_notify_uplink_csa(struct hostapd_iface *hapd, int event, u8 channel,
 				     int freq, int csa_count, u8 new_ch_width,
 				     u8 ch_seg_0, u8 ch_seg_1,
