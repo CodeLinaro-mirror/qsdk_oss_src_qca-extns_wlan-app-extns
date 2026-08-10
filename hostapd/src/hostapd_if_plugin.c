bd74eee9dc4f4e4150c22699533f6c43371c3f20
Arpitha Ramesha <arpirame@qti.qualcomm.com>
2026-08-10 13:42:50 -0700

    QSDK:HOSTAPD:hostapd-extn: Fix eloop plugin init return type
    eloop_type is now set in hostapd_if_init based on the getopt flag.
    The return of hostapd_if_plugin_init is not required. Drop the
    unused enum return and the stray HOSTAPD_IF_ELOOP_ROUTING.
    
    Add a debug log hostapd_if_eloop_init() to state direct_call eloop_type
    when chosen
    
    Change-Id: Ic48ec2ebd4198524d982ff9486772e5aadeeb951
    Signed-off-by: Arpitha Ramesha <arpirame@qti.qualcomm.com>

--- hostapd/src/hostapd_if_plugin.c
+++ hostapd/src/hostapd_if_plugin.c
@@ -1417,7 +1417,7 @@ void hostapd_if_plugin_deinit()
 
 static pthread_t invoke_thread;
 /* Constructor: called when the shared library is loaded */
-enum hostapd_if_eloop_type hostapd_if_plugin_init(void *arg)
+void hostapd_if_plugin_init(void *arg)
 {
 	global_conf.assoc.status_code = -1;
 	global_conf.auth.status_code = -1;
@@ -1456,7 +1456,6 @@ enum hostapd_if_eloop_type hostapd_if_plugin_init(void *arg)
 	test_plugin.pull_pmk_r1          = pull_pmk_r1;
 
 	hostapd_plugin_register(&test_plugin);
-	return HOSTAPD_IF_ELOOP_ROUTING;
 }
 
 static void hostapd_if_plugin_set_assoc_resp_ies(uint8_t *buf, size_t buf_len)
